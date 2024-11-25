8f498c1c12b49262984426751f8ea379b322bd5f
ahmadsaad2 <137313539+ahmadsaad2@users.noreply.github.com>
2024-11-25 17:41:24 +0200

    home page edit
    front

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -1,20 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+import 'package:sayyarti/Screens/Welcome/welcome_screen.dart';
+import 'package:sayyarti/constants.dart';
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sayyarti',
+      theme: ThemeData(
+          primaryColor: const Color.fromARGB(255, 8, 75, 219),
+          scaffoldBackgroundColor: Colors.white,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              elevation: 0,
+              foregroundColor: Colors.white,
+              backgroundColor: const Color.fromARGB(255, 7, 44, 146),
+              shape: const StadiumBorder(),
+              maximumSize: const Size(double.infinity, 56),
+              minimumSize: const Size(double.infinity, 56),
+            ),
+          ),
+          inputDecorationTheme: const InputDecorationTheme(
+            filled: true,
+            fillColor: kPrimaryLightColor,
+            iconColor: Color.fromRGBO(13, 11, 134, 1),
+            prefixIconColor: Color.fromARGB(255, 0, 24, 161),
+            contentPadding: EdgeInsets.symmetric(
+                horizontal: defaultPadding, vertical: defaultPadding),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30)),
+              borderSide: BorderSide.none,
+            ),
+          )),
+      home: const WelcomeScreen(),
     );
   }
 }
