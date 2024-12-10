f6bd96aec60ece4cc51613af76bb8d8620106cb2
malhaq <mohammadabedalhaq02@gmail.com>
2024-12-10 06:46:20 +0200

    adding the maps to get current location and chosen location on the map

---
+++ front/lib/model/place.dart
@@ -0,0 +1,11 @@
+class Place {
+  const Place({
+    required this.latitude,
+    required this.longitude,
+    required this.address,
+  });
+
+  final double latitude;
+  final double longitude;
+  final String address;
+}
